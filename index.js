c83227c440fee6273190825bc6b4ad7203d366ec
kevin <kevinr060799@gmaail.com>
2024-10-13 01:15:36 -0500

    se modifica el cors para recibir informacion del frontend

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
 const express = require('express');
-const {urlencoded, json} = require('express');
+const { urlencoded, json } = require('express');
 const router = require('./routes/calculadora.routes.js');
 const cors = require('cors');
 
 const app = express();
 
-app.use(urlencoded({extended: true}));
+app.use(urlencoded({ extended: true }));
 app.use(json());
-app.use(cors());
 
-app.get('/', async (req,res) =>{
+// Configuración de CORS para permitir solicitudes desde el frontend
+app.use(cors({
+    origin: 'https://calculadora-front-ecru.vercel.app',
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type'],
+}));
+
+app.get('/', async (req, res) => {
     res.send("hola soy el back de la calculadora de Kevin");
-})
+});
 
 app.use('/v1/calculadora', router);
 
-app.listen(3500, ()=>{
+app.listen(3500, () => {
     console.log("Listening at port 3500");
-})
+});
